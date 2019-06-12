776680638e8b9424a9b7cc079f9ca5c547780d2a
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-06-12 15:34:34 +0200

    Rename application entry point class

--- src/main/java/io/archilab/prox/apigateway/ApiGateway.java
+++ src/main/java/io/archilab/prox/apigateway/ApiGateway.java
@@ -8,10 +8,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-public class ApiGatewayApplication {
+public class ApiGateway {
 
   public static void main(String[] args) {
-    SpringApplication.run(ApiGatewayApplication.class, args);
+    SpringApplication.run(ApiGateway.class, args);
   }
 
 }
