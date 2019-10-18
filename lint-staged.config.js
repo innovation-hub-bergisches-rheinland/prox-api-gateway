681693c60ffc85e575a7a4f45e52aad6a079efc3
Julian Lengelsen <julian.lengelsen@th-koeln.de>
2019-10-18 14:45:18 +0200

    Fix lint-staged.config.js for Windows

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,10 @@
 const path = require("path");
 module.exports = {
   "*.java": [
-    absolutePaths =>
-      `mvn spotless:apply -DspotlessFiles=${absolutePaths.join(",")}`,
+    absolutePaths => {
+      const resolvedPaths = absolutePaths.map(file => path.resolve(file));
+      return `mvn spotless:apply -DspotlessFiles=${resolvedPaths.join(",")}`;
+    },
     "git add"
   ],
   "*.{js,ts,css,scss,json,md,html,yml,yaml}": ["prettier --write", "git add"]
