22c2cf0957443f130989f13853cfbbb64b2c3d38
Tobias Hund <tobias.hund@innovation-hub.de>
2020-12-01 18:18:49 +0100

    swap scheduled job to middleware implementation

--- src/main/java/de/innovationhub/prox/apigateway/ApiGateway.java
+++ src/main/java/de/innovationhub/prox/apigateway/ApiGateway.java
@@ -28,12 +28,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableZuulProxy
-@EnableScheduling
 public class ApiGateway {
 
   public static void main(String[] args) {
