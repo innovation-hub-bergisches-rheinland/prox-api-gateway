a5462d0863a5ede1f5159cfe5d2eea7875c2f306
Tobias Hund <tobias.hund@innovation-hub.de>
2021-11-17 15:48:15 +0100

    Remove windows path delimiter substitution
    Seems to be unnecessary

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,22 +1,7 @@
-const path = require("path");
-
 module.exports = {
   // Delegate the whole formatting to spotless
   "*": [
-    (absolutePaths) => {
-      let resolvedPaths = absolutePaths;
-
-      if (process.platform === "win32") {
-        console.log("ABCDEFG");
-        resolvedPaths = absolutePaths
-          .map((file) => path.resolve(file))
-          .map((file) => file.split("\\").join("\\\\"));
-        console.log(resolvedPaths);
-        return `./mvnw.cmd spotless:apply -X -DspotlessFiles=${resolvedPaths.join(
-          ","
-        )}`;
-      }
-
+    (resolvedPaths) => {
       return `./mvnw spotless:apply -X -DspotlessFiles=${resolvedPaths.join(
         ","
       )}`;
