1cd4fae327d5a12dbed65e6cf70b171a02092ebf
Tobias Hund <tobias.hund@innovation-hub.de>
2021-12-02 11:58:56 +0100

    fix: add Context loads test

---
+++ src/test/java/de/innovationhub/prox/apigateway/ApiGatewayTest.java
@@ -0,0 +1,11 @@
+package de.innovationhub.prox.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayTest {
+
+  @Test
+  void contextLoads() {}
+}
