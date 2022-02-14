57862e149fb6752370407fb1d28ce96a5fbccde9
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-02-14 21:50:26 +0100

    style: apply formatting

--- src/main/java/de/innovationhub/prox/apigateway/OpenApiController.java
+++ src/main/java/de/innovationhub/prox/apigateway/OpenApiController.java
@@ -1,13 +1,11 @@
 package de.innovationhub.prox.apigateway;
 
 
-import com.netflix.appinfo.InstanceInfo;
 import com.netflix.discovery.EurekaClient;
 import java.net.URI;
 import java.net.URISyntaxException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.handler.FilteringWebHandler;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -21,16 +19,13 @@ import reactor.core.publisher.Mono;
 @RestController
 @Slf4j
 public class OpenApiController {
-  private final static String METADATA_CONFIG_KEY = "openApiPath";
+  private static final String METADATA_CONFIG_KEY = "openApiPath";
   private final WebClient webClient;
   private final RouteLocator routeLocator;
   private final EurekaClient eurekaClient;
 
   @Autowired
-  public OpenApiController(
-      RouteLocator routeLocator,
-      EurekaClient eurekaClient
-  ) {
+  public OpenApiController(RouteLocator routeLocator, EurekaClient eurekaClient) {
     this.routeLocator = routeLocator;
     this.eurekaClient = eurekaClient;
     this.webClient = WebClient.builder().build();
@@ -40,18 +35,25 @@ public class OpenApiController {
   public Mono<ResponseEntity<String>> getOpenApiDefinitionFromService(
       @RequestParam("group") String group, ServerWebExchange serverWebExchange) {
     return this.getOpenApiUrl(group)
-        .flatMap(it -> this.webClient.get().uri(it)
-        .accept(MediaType.APPLICATION_JSON)
-        .exchangeToMono(res -> res.bodyToMono(String.class))
-        .map(ResponseEntity::ok));
+        .flatMap(
+            it ->
+                this.webClient
+                    .get()
+                    .uri(it)
+                    .accept(MediaType.APPLICATION_JSON)
+                    .exchangeToMono(res -> res.bodyToMono(String.class))
+                    .map(ResponseEntity::ok));
   }
 
   private Mono<URI> getOpenApiUrl(String serviceName) {
-    return this.routeLocator.getRoutes()
+    return this.routeLocator
+        .getRoutes()
         .filter(it -> it.getId().equals(serviceName))
         .filter(it -> it.getMetadata().get(METADATA_CONFIG_KEY) instanceof String)
-        .flatMap(it -> resolveUrl(it.getUri())
-            .map(uri -> uri.resolve((String) it.getMetadata().get(METADATA_CONFIG_KEY))))
+        .flatMap(
+            it ->
+                resolveUrl(it.getUri())
+                    .map(uri -> uri.resolve((String) it.getMetadata().get(METADATA_CONFIG_KEY))))
         .next();
   }
 
@@ -59,16 +61,18 @@ public class OpenApiController {
   //  filters to webclient requests. However, I don't have any clue how to do this and as we're
   //  probably replacing Eureka with k8s native load-balancing I didn't put much effort in it
   private Mono<URI> resolveUrl(URI serviceUri) {
-    if(serviceUri.getScheme().equals("lb")) {
-      return Mono.fromCallable(() -> eurekaClient.getNextServerFromEureka(serviceUri.getHost(), false))
-          .map(instance -> {
-            try {
-              return new URI(instance.getHomePageUrl());
-            } catch (URISyntaxException e) {
-              e.printStackTrace();
-              throw new RuntimeException(e);
-            }
-          });
+    if (serviceUri.getScheme().equals("lb")) {
+      return Mono.fromCallable(
+              () -> eurekaClient.getNextServerFromEureka(serviceUri.getHost(), false))
+          .map(
+              instance -> {
+                try {
+                  return new URI(instance.getHomePageUrl());
+                } catch (URISyntaxException e) {
+                  e.printStackTrace();
+                  throw new RuntimeException(e);
+                }
+              });
     }
     return Mono.just(serviceUri);
   }
--- src/main/java/de/innovationhub/prox/apigateway/SwaggerConfig.java
+++ src/main/java/de/innovationhub/prox/apigateway/SwaggerConfig.java
@@ -1,32 +1,32 @@
 package de.innovationhub.prox.apigateway;
 
 
-import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.stream.Collectors;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
-import springfox.boot.starter.autoconfigure.SpringfoxConfigurationProperties.OpenApiConfigurationProperties;
 import springfox.documentation.swagger.web.InMemorySwaggerResourcesProvider;
 import springfox.documentation.swagger.web.SwaggerResource;
 import springfox.documentation.swagger.web.SwaggerResourcesProvider;
 
 @Configuration
 public class SwaggerConfig {
-  private final static String METADATA_CONFIG_KEY = "openApiPath";
+  private static final String METADATA_CONFIG_KEY = "openApiPath";
   private final List<String> SERVICES_TO_INCLUDE;
 
   public SwaggerConfig(RouteLocator routeLocator) {
     try {
-      this.SERVICES_TO_INCLUDE = routeLocator.getRoutes()
-          .filter(it -> it.getMetadata().get(METADATA_CONFIG_KEY) instanceof String)
-          .map(it -> it.getId())
-          .collectList().toFuture().get();
+      this.SERVICES_TO_INCLUDE =
+          routeLocator
+              .getRoutes()
+              .filter(it -> it.getMetadata().get(METADATA_CONFIG_KEY) instanceof String)
+              .map(it -> it.getId())
+              .collectList()
+              .toFuture()
+              .get();
     } catch (InterruptedException | ExecutionException e) {
       e.printStackTrace();
       throw new RuntimeException(e);
