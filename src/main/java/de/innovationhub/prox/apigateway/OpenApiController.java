0fe61c14e5814e05cb888f046eb3585f81f9d772
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-02-14 21:24:57 +0100

    refactor: use metadata instead of configuration properties

--- src/main/java/de/innovationhub/prox/apigateway/OpenApiController.java
+++ src/main/java/de/innovationhub/prox/apigateway/OpenApiController.java
@@ -4,31 +4,26 @@ package de.innovationhub.prox.apigateway;
 import java.net.URI;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.gateway.route.RouteDefinition;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
-import org.springframework.http.HttpStatus;
+import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
 @RestController
 @Slf4j
 public class OpenApiController {
+  private final static String METADATA_CONFIG_KEY = "openApiPath";
   private final WebClient webClient;
-  private final RouteDefinitionLocator routeDefinitionLocator;
-  private final OpenApiConfigurationProperties openApiConfigurationProperties;
+  private final RouteLocator routeLocator;
 
   @Autowired
   public OpenApiController(
-      RouteDefinitionLocator routeDefinitionLocator,
-      OpenApiConfigurationProperties openApiConfigurationProperties) {
-    this.routeDefinitionLocator = routeDefinitionLocator;
-    this.openApiConfigurationProperties = openApiConfigurationProperties;
+      RouteLocator routeLocator) {
+    this.routeLocator = routeLocator;
     this.webClient = WebClient.builder().build();
   }
 
@@ -43,23 +38,9 @@ public class OpenApiController {
   }
 
   private Mono<URI> getOpenApiUrl(String serviceName) {
-    var definition = this.openApiConfigurationProperties.getDefinitions().get(serviceName);
-    if(definition == null) {
-      log.error("No Definition found for service " + serviceName);
-      return Mono.error(new RuntimeException("No Definition found for service " + serviceName));
-    }
-    if(definition.getPath() == null) {
-      log.error("No OpenAPI Path defined for service " + serviceName);
-      return Mono.error(new RuntimeException("No OpenAPI Path defined for service " + serviceName));
-    }
-    return this.locateService(serviceName)
-        .map(s -> s.resolve(definition.getPath()));
-  }
-
-  private Mono<URI> locateService(String serviceName) {
-    return this.routeDefinitionLocator.getRouteDefinitions()
-        .filter(it -> it.getId().equals(serviceName))
-        .map(RouteDefinition::getUri)
+    return this.routeLocator.getRoutes()
+        .filter(it -> it.getMetadata().get(METADATA_CONFIG_KEY) instanceof String)
+        .map(it -> it.getUri().resolve((String) it.getMetadata().get(METADATA_CONFIG_KEY)))
         .next();
   }
 }
