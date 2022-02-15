3a58f9f130de39e0025d1dfbcbeaac93fd557e96
tobi6112 <22715034+tobi6112@users.noreply.github.com>
2022-02-15 18:15:25 +0100

    fix: remove obsolete eureka code parts

--- src/main/java/de/innovationhub/prox/apigateway/ApiGateway.java
+++ src/main/java/de/innovationhub/prox/apigateway/ApiGateway.java
@@ -3,10 +3,8 @@ package de.innovationhub.prox.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ApiGateway {
 
   public static void main(String[] args) {
